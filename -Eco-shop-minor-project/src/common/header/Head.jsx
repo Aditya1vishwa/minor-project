d9e93bb2b3096616ab123356340c3e5a1726c26a
aaditya <aadityavishwakarma708@gmail.com>
2022-09-15 19:28:05 +0530

    first

---
+++ -Eco-shop-minor-project/src/common/header/Head.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +916264673625</label>
+            <i className='fa fa-envelope'></i>
+            <label>aadityavishwakarma@708.com</label>
+          </div>
+          <div className='right row RText'>
+           
+          <a href="https://www.linkedin.com/in/aditya-vishwakarma-293493201">know us</a>
+            <label>Need Help?</label>
+            <span>🔥</span>
+            <label>Indian</label>
+            <span>💵</span>
+            <label>Rupee</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
---
+++ -Eco-shop-minor-project/src/components/MainPage/Categories.jsx
@@ -0,0 +1,64 @@
+import React from "react"
+
+const Categories = () => {
+  const data = [
+    {
+      cateImg: "./images/category/cat1.png",
+      cateName: "pens",
+    },
+    {
+      cateImg: "./images/category/cat2.png",
+      cateName: "groceries",
+    },
+    {
+      cateImg: "./images/category/cat3.png",
+      cateName: "decorate things",
+    },
+    {
+      cateImg: "./images/category/cat4.png",
+      cateName: "Home & Garden",
+    },
+    {
+      cateImg: "./images/category/cat5.png",
+      cateName: "Gifts",
+    },
+    {
+      cateImg: "./images/category/cat6.png",
+      cateName: "Music",
+    },
+    {
+      cateImg: "./images/category/cat7.png",
+      cateName: "Health & Beauty",
+    },
+   
+    {
+      cateImg: "./images/category/cat9.png",
+      cateName: "Baby Toys",
+    },
+    {
+      cateImg: "./images/category/cat10.png",
+      cateName: "Groceries",
+    },
+    {
+      cateImg: "./images/category/cat11.png",
+      cateName: "Books",
+    },
+  ]
+
+  return (
+    <>
+      <div className='category'>
+        {data.map((value, index) => {
+          return (
+            <div className='box f_flex' key={index}>
+              <img src={value.cateImg} alt='' />
+              <span>{value.cateName}</span>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+
+export default Categories
